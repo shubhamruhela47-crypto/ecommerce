f36a444114b169d5614cce6889d3d6f7f66fec3e
shubhamruhela47-crypto <shubhamruhela47@gmail.com>
2026-06-29 14:36:21 +0530

    Update shop.jsx

--- frontend/src/pages/shop.jsx
+++ frontend/src/pages/shop.jsx
@@ -8,7 +8,7 @@ const Shop = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch("/api/products");
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/products`)
         const data = await res.json();
 
         setProducts(Array.isArray(data) ? data : []);
